7e3a4f1a5e63e63f22673b37bcd52955780e9541
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-03-20 17:52:47 +0900

    Initial commit

---
+++ src/main/java/hwanhee/DevSketch_Backend/DevSketchBackendApplication.java
@@ -0,0 +1,13 @@
+package hwanhee.DevSketch_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSketchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSketchBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hwanhee/DevSketch_Backend/DevSketchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hwanhee.DevSketch_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSketchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
